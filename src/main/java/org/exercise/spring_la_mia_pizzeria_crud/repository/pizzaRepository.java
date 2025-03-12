babdf941d8032596dc5a5a29e9973577e2b2b9f6
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-12 23:32:40 +0100

    add pizzaRepository

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/repository/pizzaRepository.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/repository/pizzaRepository.java
@@ -2,5 +2,5 @@ package org.exercise.spring_la_mia_pizzeria_crud.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.exercise.spring_la_mia_pizzeria_crud.entity.Pizza;
 
-public interface pizzaRepository extends JpaRepository<Pizza, Long> {
+public interface PizzaRepository extends JpaRepository<Pizza, String> {
 } 
