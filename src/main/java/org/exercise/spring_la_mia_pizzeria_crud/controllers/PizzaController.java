0d97442f6a1eb42a529ac2482835643d967b58f6
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-13 12:20:04 +0100

    add the show and changes into pizza attributes

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -4,25 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
-
 import org.exercise.spring_la_mia_pizzeria_crud.entity.Pizza;
 import org.exercise.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 
 @Controller
 @RequestMapping("/pizzas")
-public class indexController {
+public class PizzaController {
     @Autowired
     private PizzaRepository repo;
    
     @GetMapping 
-    public String home(Model model){
+    public String index(Model model){
         List<Pizza> pizzas = repo.findAll();
         model.addAttribute("pizzas", pizzas);
         return "crud_pages/index";
     }
 
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable("id") Integer id){
+        String index = Integer.toString(id);
+        //Pizza pizza = repo.findById(id).get(); da errore vuole uno string
+        Pizza pizza = repo.findById(index).get();
+        model.addAttribute("pizza", pizza);
+        return "crud_pages/show";
+    }
 }
 
 
--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/entity/pizza.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/entity/pizza.java
@@ -6,6 +6,9 @@ import jakarta.persistence.*;
 @Table(name = "pizzas")
 public class Pizza implements Serializable{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name", nullable = false)
     private String name;
     @Column(name = "description", nullable = false)
     private String description;
@@ -15,11 +18,19 @@ public class Pizza implements Serializable{
     private float price;
 
     public Pizza() {}
-    public Pizza(String name, String description, String url, float price) {
+    public Pizza(String name, String description, String url, float price, int id) {
         this.name = name;
         this.description = description;
         this.url = url;
         this.price = price;
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -56,7 +67,8 @@ public class Pizza implements Serializable{
 
     @Override
     public String toString() {
-        return "pizza: name= " + name + ", description= " + description + ", url= " + url + ", price= " + price + "]";
+        return "Pizza [id=" + id + ", name=" + name + ", description=" + description + ", url=" + url + ", price="
+                + price + "]";
     }
     
 }
