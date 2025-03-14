ece6fc656cf9d9e05ddf4c2505565d0d661b75e1
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-14 16:14:00 +0100

    add a method for filter

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/repository/pizzaRepository.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/repository/pizzaRepository.java
@@ -1,6 +1,10 @@
 package org.exercise.spring_la_mia_pizzeria_crud.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 import org.exercise.spring_la_mia_pizzeria_crud.entity.Pizza;
 
-public interface PizzaRepository extends JpaRepository<Pizza, String> {
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+    public List<Pizza> findByNameContaining(String str);
 } 
