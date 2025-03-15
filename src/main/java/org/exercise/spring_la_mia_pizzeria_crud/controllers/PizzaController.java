c46ea7b1668da67f4aa6e346d2744e9fa4136734
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-15 17:22:38 +0100

    change imports

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
-import org.exercise.spring_la_mia_pizzeria_crud.entity.Pizza;
+
+import org.exercise.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.exercise.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/pizzas")
@@ -36,6 +41,18 @@ public class PizzaController {
         Pizza pizza = repo.findById(id).get();
         model.addAttribute("pizza", pizza);
         return "crud_pages/show";
+    }
+
+
+    @GetMapping("/create")
+    public String create(Model model, @ModelAttribute Pizza pizza) {
+        model.addAttribute("pizza", pizza);
+        return "create";
+    }
+
+    @PostMapping("path")
+    public String postMethodName(@RequestBody String entity) {    
+        return entity;
     }
 }
 
--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/repository/pizzaRepository.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/repository/pizzaRepository.java
@@ -1,9 +1,8 @@
 package org.exercise.spring_la_mia_pizzeria_crud.repository;
+import org.exercise.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
-import org.exercise.spring_la_mia_pizzeria_crud.entity.Pizza;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
     public List<Pizza> findByNameContaining(String str);
