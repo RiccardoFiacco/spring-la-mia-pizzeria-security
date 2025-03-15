e26745c44a248c087b8832739ee9b5eccbe4aebc
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-15 17:59:12 +0100

    add the routes for the create method

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -3,16 +3,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.validation.Valid;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
-
 import org.exercise.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.exercise.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -45,14 +46,20 @@ public class PizzaController {
 
 
     @GetMapping("/create")
-    public String create(Model model, @ModelAttribute Pizza pizza) {
-        model.addAttribute("pizza", pizza);
-        return "create";
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "crud_pages/create";
     }
 
-    @PostMapping("path")
-    public String postMethodName(@RequestBody String entity) {    
-        return entity;
+    @PostMapping("/create")
+    public String postMethodName(Model m, @Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult br){
+        
+        if(br.hasErrors()){
+            return "crud_pages/create";
+        }
+
+        repo.save(formPizza);
+        return "redirect:/pizzas";
     }
 }
 
