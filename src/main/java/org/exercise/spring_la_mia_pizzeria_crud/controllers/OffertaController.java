04f8f8332f893f5de7b1184e4feb2c707595b5f6
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-24 22:08:44 +0100

    add update route

--- src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/OffertaController.java
+++ src/main/java/org/exercise/spring_la_mia_pizzeria_crud/controllers/OffertaController.java
@@ -2,13 +2,13 @@ package org.exercise.spring_la_mia_pizzeria_crud.controllers;
 import org.exercise.spring_la_mia_pizzeria_crud.repository.OffertaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import ch.qos.logback.core.model.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import jakarta.validation.Valid;
 import org.exercise.spring_la_mia_pizzeria_crud.model.Offerta;
 
@@ -18,8 +18,29 @@ public class OffertaController {
 
     @Autowired
     private OffertaRepository offertRepository;
+
     @PostMapping("/create")
-    public String getMethodName(Model model, 
+    public String create(Model model, 
+    @Valid @ModelAttribute("offert") Offerta offert, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "offerts_crud_pages/create";
+        }
+        int id = offert.getPizza().getId();
+        offertRepository.save(offert);
+        return "redirect:/pizzas/" + id;
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateForm(Model model, @PathVariable int id) {
+        Offerta offert = offertRepository.findById(id).get();
+        model.addAttribute("offert", offert);
+        model.addAttribute("flag", true);
+        return "offerts_crud_pages/offerts_form";
+    }
+
+
+    @PostMapping("/update/{id}")
+    public String update(Model model, 
     @Valid @ModelAttribute("offert") Offerta offert, BindingResult bindingResult) {
         if(bindingResult.hasErrors()){
             return "offerts_crud_pages/create";
